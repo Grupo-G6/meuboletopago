0e7fb9fd9d6c633462433a7a684cc23a80dd7828
dev-leticia-costa <leticiaccmarques@hotmail.com>
2023-02-15 16:37:02 -0300

    Api

--- shared/src/commonMain/kotlin/br/com/digitalhouse/meuboletopago/model/Login.kt
+++ shared/src/commonMain/kotlin/br/com/digitalhouse/meuboletopago/model/Login.kt
@@ -1,7 +1,8 @@
 package br.com.digitalhouse.meuboletopago.model
 
-class Login ( val loginEnter:String, val senha: String ) {
-        fun validator() = (loginEnter == "usuario@kmm.com" && senha == "12345")
-//        fun validator() = (login == "" && senha == "")
+@kotlinx.serialization.Serializable
+class Login ( val email:String, val password: String ) {
+        fun validator() = (email == "usuario@kmm.com" && password== "12345")
+
 
 }
